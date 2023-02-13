c9efdb57a22783cc33b7764264d09aeb31e476bd
Tamas Toth <tamas.toth@runtimeverification.com>
2023-02-13 16:56:44 +0100

    Add fixtures for kompiled `kbuild` targets

---
+++ kmir/src/tests/conftest.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+from pyk.kbuild import KBuild, Package
+from pytest import TempPathFactory
+
+
+@pytest.fixture(scope='session')
+def kbuild(tmp_path_factory: TempPathFactory) -> KBuild:
+    return KBuild(tmp_path_factory.mktemp('kbuild'))
+
+
+@pytest.fixture(scope='session')
+def package() -> Package:
+    return Package.create(Path('kbuild.toml'))
+
+
+@pytest.fixture(scope='session')
+def llvm_dir(kbuild: KBuild, package: Package) -> Path:
+    return kbuild.kompile(package, 'llvm')
+
+
+@pytest.fixture(scope='session')
+def haskell_dir(kbuild: KBuild, package: Package) -> Path:
+    return kbuild.kompile(package, 'haskell')
--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -1,2 +1,9 @@
-def test_integration() -> None:
-    pass
+from pathlib import Path
+
+
+def test_llvm(llvm_dir: Path) -> None:
+    assert llvm_dir.is_dir()
+
+
+def test_haskell(haskell_dir: Path) -> None:
+    assert haskell_dir.is_dir()
