b82449d16db112915cf3051533867b65811208aa
Virgil <virgil.serbanuta@runtimeverification.com>
2023-02-14 18:53:23 +0200

    Use _kast from pyk

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -1,7 +1,8 @@
-import subprocess
 from pathlib import Path
 
+import pyk.ktool.kprint as kprint
 import pytest
+from pyk.ktool.kprint import KAstInput, KAstOutput
 
 HANDWRITTEN_SYNTAX_DIR = Path(__file__).parent / 'test-data' / 'parsing' / 'handwritten-syntax'
 HANDWRITTEN_SYNTAX_FILES = list(HANDWRITTEN_SYNTAX_DIR.glob('*.mir'))
@@ -15,15 +16,10 @@ def test_haskell(haskell_dir: Path) -> None:
     assert haskell_dir.is_dir()
 
 
-def kast(definition_dir: Path, input_name: Path) -> None:
-    args = ['kast', '--output', 'kore', '--sort', 'Mir', '--definition', str(definition_dir), str(input_name)]
-    result = subprocess.run(args)
-    if result.returncode != 0:
-        raise Exception('%s returned %d' % (args, result.returncode))
-
-
 @pytest.mark.parametrize('input_path', HANDWRITTEN_SYNTAX_FILES, ids=[str(f.name) for f in HANDWRITTEN_SYNTAX_FILES])
 def test_handwritten_syntax(haskell_dir: Path, input_path: Path) -> None:
-    definition_dir = haskell_dir
+    pass
 
-    kast(definition_dir, input_path)
+    kprint._kast(
+        definition_dir=haskell_dir, input_file=input_path, input=KAstInput.PROGRAM, output=KAstOutput.KORE, sort='Mir'
+    )
