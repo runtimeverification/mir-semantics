9579cd1a5603fdb828b79750d6fa10376d297486
Virgil <virgil.serbanuta@runtimeverification.com>
2023-02-14 18:08:36 +0200

    Fix review comments

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -5,7 +5,8 @@ import pytest
 
 
 
-HANDWRITTEN_SYNTAX_DIR = Path('src') / 'tests' / 'integration' / 'test-data' / 'parsing' / 'handwritten-syntax'
+HANDWRITTEN_SYNTAX_DIR = Path(__file__).parent / 'test-data' / 'parsing' / 'handwritten-syntax'
+HANDWRITTEN_SYNTAX_FILES = list(HANDWRITTEN_SYNTAX_DIR.glob('*.mir'))
 
 
 def test_llvm(llvm_dir: Path) -> None:
@@ -23,7 +24,10 @@ def kast(definition_dir: Path, input_name: Path) -> None:
         raise Exception('%s returned %d' % (args, result.returncode))
 
 
-@pytest.mark.parametrize('input_path', list(HANDWRITTEN_SYNTAX_DIR.glob('*.mir')))
+@pytest.mark.parametrize(
+        'input_path',
+        HANDWRITTEN_SYNTAX_FILES,
+        ids = [str(f.name) for f in HANDWRITTEN_SYNTAX_FILES])
 def test_handwritten_syntax(haskell_dir: Path, input_path: Path) -> None:
     definition_dir = haskell_dir
 
