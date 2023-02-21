7006c110793ee272dc205271c4b6a9c64aeb3ef3
Yan <40077433+yanliu18@users.noreply.github.com>
2023-02-21 17:14:02 +1100

    Revert "Include preprocessing step in `parse_program` (#40)"
    This reverts commit 389435f4c03b89d1a23f0eae0f8c602071c64a7d.

--- kmir/src/kmir/kmir.py
+++ kmir/src/kmir/kmir.py
@@ -4,14 +4,11 @@ import json
 from dataclasses import dataclass
 from pathlib import Path
 from subprocess import CalledProcessError
-from tempfile import NamedTemporaryFile
-from typing import Optional, Union, final
+from typing import Union, final
 
 from pyk.cli_utils import check_dir_path, check_file_path
 from pyk.kast.inner import KInner
 from pyk.ktool.kprint import KAstInput, KAstOutput, _kast
-
-from .preprocessor import preprocess
 
 
 @final
@@ -30,37 +27,18 @@ class KMIR:
         object.__setattr__(self, 'llvm_dir', llvm_dir)
         object.__setattr__(self, 'haskell_dir', haskell_dir)
 
-    def parse_program(
-        self,
-        program_file: Union[str, Path],
-        *,
-        temp_file: Optional[Union[str, Path]] = None,
-    ) -> KInner:
-        def parse(program_file: Path) -> KInner:
-            try:
-                proc_res = _kast(
-                    definition_dir=self.llvm_dir,
-                    input_file=program_file,
-                    input=KAstInput.PROGRAM,
-                    output=KAstOutput.JSON,
-                    sort='Mir',
-                )
-            except CalledProcessError as err:
-                raise ValueError("Couldn't parse program") from err
-
-            return KInner.from_dict(json.loads(proc_res.stdout)['term'])
-
-        def preprocess_and_parse(program_file: Path, temp_file: Path) -> KInner:
-            temp_file.write_text(preprocess(program_file.read_text()))
-            return parse(temp_file)
-
+    def parse_program(self, program_file: Union[str, Path]) -> KInner:
         program_file = Path(program_file)
         check_file_path(program_file)
+        try:
+            proc_res = _kast(
+                definition_dir=self.llvm_dir,
+                input_file=program_file,
+                input=KAstInput.PROGRAM,
+                output=KAstOutput.JSON,
+                sort='Mir',
+            )
+        except CalledProcessError as err:
+            raise ValueError("Couldn't parse program") from err
 
-        if temp_file is None:
-            with NamedTemporaryFile(mode='w') as f:
-                temp_file = Path(f.name)
-                return preprocess_and_parse(program_file, temp_file)
-
-        temp_file = Path(temp_file)
-        return preprocess_and_parse(program_file, temp_file)
+        return KInner.from_dict(json.loads(proc_res.stdout)['term'])
--- kmir/src/kmir/preprocessor.py
+++ kmir/src/kmir/preprocessor.py
@@ -1,19 +1,12 @@
+#!/usr/bin/env python3
+
 import re
+import sys
+import typing
 
 LINE_COMMENT_REGEXP = re.compile(r'^((?:[^/"]|/[^/"]|/?"(?:[^\\"]|\\.)*")*)//.*$')
 HEX_CLEANUP_SUFFIX = re.compile(r'^(\s*(?: [0-9a-fA-F][0-9a-fA-F])+)\s+│.*$')
 HEX_CLEANUP_SEPARATOR = re.compile(r'^(\s+0x[0-9a-fA-F]+\s+)│(\s*(?: [0-9a-fA-F][0-9a-fA-F])+)\s+│.*$')
-
-
-def preprocess(program_text: str) -> str:
-    def process_line(line: str) -> str:
-        line = line.rstrip()
-        line = remove_comments(line)
-        line = cleanup_hex_dump(line)
-        line = line.rstrip()
-        return line
-
-    return '\n'.join(process_line(line) for line in program_text.splitlines())
 
 
 def remove_comments(line: str) -> str:
@@ -31,3 +24,34 @@ def cleanup_hex_dump(line: str) -> str:
             return line
         return '%s|%s' % (m.group(1), m.group(2))
     return m.group(1)
+
+
+def preprocess(input_file: str, output_file: str) -> None:
+    with open(input_file, 'r') as f:
+        with open(output_file, 'w') as g:
+            for line in f:
+                line = remove_comments(line.rstrip())
+                line = cleanup_hex_dump(line)
+                g.write(line.rstrip())
+                g.write('\n')
+
+
+def main(args: typing.List[str]) -> None:
+    # print(remove_comments('Hello World'))
+    # print(remove_comments('Hello // World'))
+    # print(remove_comments('Hello "// World"'))
+    # print(remove_comments('Hello "\\"// World"'))
+    # print(remove_comments('Hello "// World" // Here'))
+    # print(cleanup_hex_dump('Hello world'))
+    # print(cleanup_hex_dump('  00 00 00 00 00 00 00 00                         │ ........'))
+    # print(cleanup_hex_dump('  0x00 │ 00 00 00 00 00 00 00 00                         │ ........'))
+    if len(args) != 2:
+        print('Usage: preprocessor.py input-file output-file')
+        sys.exit(1)
+    input_file = args[0]
+    output_file = args[1]
+    preprocess(input_file, output_file)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
