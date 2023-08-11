1b1db27f693c3e35caa7b4f5b4ae44b375f38f27
dkcumming <daniel.cumming@runtimeverification.com>
2023-08-11 16:45:01 +1000

    Updated python, description, overlays.default

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = " A flake for KMIR Semantics";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    let
+      allOverlays = [
+        poetry2nix.overlay
+        (final: prev: {
+          kmir = prev.poetry2nix.mkPoetryApplication {
+            python = prev.python310;
+            projectDir = ./kmir;
+            groups = [];
+            # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
+            checkGroups = [];
+           };
+        })
+      ];
+    in flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = allOverlays;
+        };
+      in {
+        packages = rec {
+          inherit (pkgs) kmir;
+          default = kmir;
+        };
+      }) // {
+        overlays.default = nixpkgs.lib.composeManyExtensions allOverlays;
+      };
+}
