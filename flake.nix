26ca6d62846a1b2a3fc5c6a46fd19dd1c8cb034e
dkcumming <daniel.cumming@runtimeverification.com>
2023-08-11 20:11:04 +1000

    broken version: trying to create buildable flake.nix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,57 @@
   description = " A flake for KMIR Semantics";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/"; # Same as KEVM and KAVM
+    k-framework.url = "github:runtimeverification/k/v6.0.44";
+    # k-framework.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.follows = "k-framework/flake-utils";
+    rv-utils.url = "github:runtimeverification/rv-nix-tools";
+    poetry2nix.url = "github:nix-community/poetry2nix/master";
+    pyk.url = "github:runtimeverification/pyk/v0.1.401";
+    pyk.inputs.flake-utils.follows = "k-framework/flake-utils";
+    pyk.inputs.nixpkgs.follows = "k-framework/nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, k-framework, nixpkgs, flake-utils, poetry2nix, rv-utils, pyk }:
     let
       allOverlays = [
         poetry2nix.overlay
+        pyk.overlay
         (final: prev: {
-          kmir = prev.poetry2nix.mkPoetryApplication {
+          kmir-pyk = prev.poetry2nix.mkPoetryApplication {
             python = prev.python310;
             projectDir = ./kmir;
+            overrides = prev.poetry2nix.overrides.withoutDefaults
+              (finalPython: prevPython: { 
+                pyk = prev.pyk-python310; 
+                # exceptiongroup = prevPython.exceptiongroup.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.flit-scm ];
+                # });
+                # filelock = prevPython.filelock.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.hatchling ];
+                # });
+                # iniconfig = prevPython.iniconfig.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.hatchling ];
+                # });
+                # packaging = prevPython.packaging.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.flit-core ];
+                # });
+                # pathspec = prevPython.pathspec.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.flit-core ];
+                # });
+                # pluggy = prevPython.pluggy.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.setuptools ];
+                # });
+                # tomli = prevPython.tomli.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.flit-core ];
+                # });
+                # typing-extensions = prevPython.typing-extensions.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.flit-core ];
+                # });
+                # hatchling = prevPython.hatchling.overridePythonAttrs (old: {
+                #   propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ finalPython.setuptools-scm ];
+                # });
+                }
+              );
             groups = [];
             # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
             checkGroups = [];
@@ -33,8 +72,8 @@
         };
       in {
         packages = rec {
-          inherit (pkgs) kmir;
-          default = kmir;
+          inherit (pkgs) kmir-pyk;
+          default = kmir-pyk;
         };
       }) // {
         overlays.default = nixpkgs.lib.composeManyExtensions allOverlays;
