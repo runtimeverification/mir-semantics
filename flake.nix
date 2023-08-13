c2dea04585472608b013af805bfeead5be85c202
dkcumming <daniel.cumming@runtimeverification.com>
2023-08-14 09:32:54 +1000

    Changed naming

--- flake.nix
+++ flake.nix
@@ -3,9 +3,8 @@
 
   inputs = {
     nixpkgs.url =
-      "github:NixOS/nixpkgs/f971f35f7d79c53a83c7b10de953f1db032cba0e"; # Same as KEVM and KAVM
+      "github:NixOS/nixpkgs/f971f35f7d79c53a83c7b10de953f1db032cba0e";
     k-framework.url = "github:runtimeverification/k/v6.0.44";
-    # k-framework.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.url = "github:runtimeverification/rv-nix-tools";
     poetry2nix.url = "github:nix-community/poetry2nix/master";
@@ -21,7 +20,7 @@
         poetry2nix.overlay
         pyk.overlay
         (final: prev: {
-          kmir-pyk = prev.poetry2nix.mkPoetryApplication {
+          kmir = prev.poetry2nix.mkPoetryApplication {
             python = final.python310;
             projectDir = ./kmir;
             overrides = prev.poetry2nix.overrides.withDefaults
@@ -62,8 +61,8 @@
         };
       in {
         packages = rec {
-          inherit (pkgs) kmir-pyk;
-          default = kmir-pyk;
+          inherit (pkgs) kmir;
+          default = kmir;
           #  = pkgs..pyk;
         };
       }) // {
