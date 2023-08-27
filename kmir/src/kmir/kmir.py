73beda472a802a272d4e20b77a1bcbd13989701f
dkcumming <daniel.cumming@runtimeverification.com>
2023-08-28 09:50:55 +1000

    Formatting

--- kmir/src/kmir/kmir.py
+++ kmir/src/kmir/kmir.py
@@ -4,7 +4,6 @@ import json
 import logging
 import os
 import subprocess
-import sys
 from dataclasses import dataclass
 from pathlib import Path
 from subprocess import CalledProcessError, CompletedProcess
