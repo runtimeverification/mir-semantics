becad2621897a1d609aa9ea5a7a0d00bdb63c437
dkcumming <daniel.cumming@runtimeverification.com>
2023-08-29 19:53:10 +1000

    formatting

--- kmir/src/tests/integration/test_prove.py
+++ kmir/src/tests/integration/test_prove.py
@@ -1,17 +1,14 @@
 import logging
-import os
 import sys
+from pathlib import Path
+from typing import Final
 
 import pytest
+from pytest import LogCaptureFixture
 
 from kmir.__main__ import exec_prove
 
 from ..utils import REPO_ROOT
-
-from pathlib import Path
-from typing import Final
-
-from pytest import LogCaptureFixture
 
 sys.setrecursionlimit(10**8)
 
@@ -23,12 +20,14 @@ TEST_DIR: Final = REPO_ROOT / 'kmir/src/tests'
 SYMBOLIC_TEST_DIR: Final = TEST_DIR / 'integration/proofs'
 INITIAL_SPECS: Final = SYMBOLIC_TEST_DIR / 'simple-spec.k'
 
-ALL_TESTS: Final = [ INITIAL_SPECS ]
+ALL_TESTS: Final = [INITIAL_SPECS]
+
 
 def exclude_list(exclude_file: Path) -> list[Path]:
     res = [REPO_ROOT / test_path for test_path in exclude_file.read_text().splitlines()]
     # assert res
     return res
+
 
 SKIPPED_TESTS: Final = exclude_list(SYMBOLIC_TEST_DIR / 'symbolic-failing')
 
@@ -37,14 +36,15 @@ SKIPPED_TESTS: Final = exclude_list(SYMBOLIC_TEST_DIR / 'symbolic-failing')
 # Pyk tests
 # ---------
 
+
 @pytest.mark.parametrize(
     'spec_file',
     ALL_TESTS,
     ids=[str(spec_file.relative_to(INITIAL_SPECS)) for spec_file in ALL_TESTS],
 )
 def test_pyk_prove(
-    llvm_dir: Path,
-    haskell_dir: Path,
+    llvm_dir: str,
+    haskell_dir: str,
     spec_file: Path,
     tmp_path: Path,
     caplog: LogCaptureFixture,
@@ -64,7 +64,7 @@ def test_pyk_prove(
         exec_prove(
             definition_dir=llvm_dir,
             haskell_dir=haskell_dir,
-            spec_file=spec_file,
+            spec_file=str(spec_file),
             save_directory=use_directory,
             smt_timeout=300,
             smt_retry_limit=10,
