9cd67231d436afce170e84ee1d1d230b693ce27a
dkcumming <daniel.cumming@runtimeverification.com>
2023-11-13 22:08:25 +1000

    [BROKEN] Added `KMIRSemantics` with explicit terminal rules.
    Error occurs from `pyk` internal call to `is_terminal`

--- kmir/src/kmir/utils.py
+++ kmir/src/kmir/utils.py
@@ -62,12 +62,13 @@ def kmir_prove(
     kcfg_explore: KCFGExplore,
     max_depth: int | None = 1000,
     max_iterations: int | None = None,
+    is_terminal: Callable[[CTerm], bool] | None = None,
     extract_branches: Callable[[CTerm], Iterable[KInner]] | None = None,
     abstract_node: Callable[[CTerm], CTerm] | None = None,
 ) -> bool:
     proof = proof
-    terminal_rules: Iterable[str] = []  # TODO terminal rules
-    cut_point_rules: Iterable[str] = []
+    terminal_rules: Iterable[str] = ['MIR.halt']
+    cut_point_rules: Iterable[str] = []  # TODO: cut point rules
     prover: APRBMCProver | APRProver | EqualityProver
     if type(proof) is APRBMCProof:
         prover = APRBMCProver(proof, kcfg_explore)
