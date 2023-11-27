0c41169420abb84fc652f5b971b754472b646e2f
yanliu18 <yan.emma.liu@gmail.com>
2023-11-27 15:17:33 +1100

    fix nix test command

--- kmir/src/tests/nix/nix-tests.sh
+++ kmir/src/tests/nix/nix-tests.sh
@@ -12,6 +12,6 @@ kmir run   --output pretty sum-to-n.mir > nix-sum-to-n.run.out
 # When the K issue below is solved, nix-asum-to-n.run.out will need to be updated
 # https://github.com/runtimeverification/k/issues/3604
 # Uncomment and run `nix build --extra-experimental-features 'nix-command flakes' --print-build-logs .#kmir-test`
-git --no-pager diff sum-to-n.run.out   sum-to-n.run.out
+git --no-pager diff nix-sum-to-n.run.out   sum-to-n.run.out
 
 kmir prove --spec-file simple-spec.k
