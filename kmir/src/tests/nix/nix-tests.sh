1c4d034c85cc01ef081ec9a1ea7857d5b3b56a5e
Yan <40077433+yanliu18@users.noreply.github.com>
2023-11-28 19:21:29 +1100

    Manual Update and minor restructure of tests (#269)
    * add the overflow test case
    
    * move overflow test case to buggy program folder
    
    * minor resutructure of tests
    
    * move overflow test case to demo folder
    
    * restructured test-prove.py test input, updated failed test list
    
    * restrcture prove test
    
    * updated manual
    
    * Manual update finish
    
    * fix type and formatting
    
    * format issue
    
    * exclude parse fail tests cases from run test
    
    * minor update of run test script
    
    * add allow skip and report file to test_prove
    
    * fix nix test command
    
    * fix nix .kmir-test build failure
    
    * fix required directory
    
    * adding links to prerequisites installation
    
    * fix comments
    
    ---------
    
    Co-authored-by: yanliu18 <yan.emma.liu@gmail.com>

--- kmir/src/tests/nix/nix-tests.sh
+++ kmir/src/tests/nix/nix-tests.sh
@@ -5,13 +5,13 @@ set -xueo pipefail
 which kmir
 kmir --help
 
-kmir parse --output pretty arithm-simple.mir > nix-arithm-simple.parse.out 
-git --no-pager diff nix-arithm-simple.parse.out arithm-simple.parse.out
+kmir parse --output pretty sum-to-n.mir > nix-sum-to-n.parse.out 
+git --no-pager diff nix-sum-to-n.parse.out sum-to-n.parse.out
 
-kmir run   --output pretty arithm-simple.mir > nix-arithm-simple.run.out 
-# When the K issue below is solved, nix-arithm-simple.run.out will need to be updated
+kmir run   --output pretty sum-to-n.mir > nix-sum-to-n.run.out 
+# When the K issue below is solved, nix-asum-to-n.run.out will need to be updated
 # https://github.com/runtimeverification/k/issues/3604
 # Uncomment and run `nix build --extra-experimental-features 'nix-command flakes' --print-build-logs .#kmir-test`
-git --no-pager diff nix-arithm-simple.run.out   arithm-simple.run.out
+git --no-pager diff nix-sum-to-n.run.out   sum-to-n.run.out
 
 kmir prove --spec-file simple-spec.k
