db4b053243040a5a0d11c92074095ccf1bb5764c
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2024-04-15 16:23:18 -0500

    Add options classes and use in functions (#349)
    * Add options classes and use in functions
    
    * Set Version: 0.2.47
    
    * Remove default values from argparse calls
    
    * Fix test
    
    * Fix output format in test
    
    * Fix test
    
    * Fix test
    
    * Wrap llvm and haskell dir in Path
    
    * Fix formatting
    
    * Try fixing compiletest-rs tests
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- kmir/src/kmir/parse.py
+++ kmir/src/kmir/parse.py
@@ -1,7 +1,10 @@
 import logging
+import os
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Final, Optional
+from typing import Any, Final, Optional
+
+from pyk.cli.args import LoggingOptions
 
 from .kmir import KMIR
 from .utils import preprocess_mir_file
@@ -10,23 +13,38 @@ _LOGGER: Final = logging.getLogger(__name__)
 _LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 
 
+class ParseOptions(LoggingOptions):
+    mir_file: Path
+    definition_dir: Path | None
+    input: str
+    output: str
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        llvm_dir_str = os.getenv('KMIR_LLVM_DIR')
+        llvm_dir = Path(llvm_dir_str) if llvm_dir_str is not None else None
+        return {
+            'definition_dir': llvm_dir,
+            'input': 'program',
+            'output': 'pretty',
+        }
+
+
 def parse(
     kmir: KMIR,
-    mir_file: Path,
+    options: ParseOptions,
     *,
-    input: str = 'program',  # Assume 'program' only, TODO: support option `rs`
-    output: str = 'none',
     temp_file: Optional[Path] = None,
 ) -> None:
     # input_format = KAstInput[input.upper()]
-    _LOGGER.info(f'Parsing {mir_file} from format {input} to format {output}')
+    _LOGGER.info(f'Parsing {options.mir_file} from format {options.input} to format {options.output}')
 
     if temp_file is None:
         with NamedTemporaryFile(mode='w') as f:
             temp_file = Path(f.name)
 
-    preprocess_mir_file(mir_file, temp_file)
+    preprocess_mir_file(options.mir_file, temp_file)
 
     proc_res = kmir.mir_to_kore(temp_file)
 
-    kmir.print_kore_to_xxx(proc_res.stdout, output)
+    kmir.print_kore_to_xxx(proc_res.stdout, options.output)
--- kmir/src/kmir/prove.py
+++ kmir/src/kmir/prove.py
@@ -1,15 +1,17 @@
 import logging
+import os
 from pathlib import Path
-from typing import Any, Final, Iterable, Optional
+from typing import Any, Final
 
+from pyk.cli.args import LoggingOptions
 from pyk.proof import APRProof
 from pyk.proof.proof import Proof
 from pyk.proof.reachability import APRFailureInfo
 from pyk.proof.show import APRProofShow
 from pyk.proof.tui import APRProofViewer
 
+from .cli import KCFGShowProofOptions
 from .kmir import KMIR
-from .utils import NodeIdLike
 
 _LOGGER: Final = logging.getLogger(__name__)
 _LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
@@ -30,17 +32,47 @@ def get_claim_labels(kmir: KMIR, spec_file: Path) -> list[str]:
     return list(all_claims.keys())
 
 
+class ProveOptions(LoggingOptions):
+    spec_file: Path
+    definition_dir: Path | None
+    haskell_dir: Path | None
+    claim_list: bool
+    claim: str | None
+    bug_report: bool
+    depth: int | None
+    smt_timeout: int
+    smt_retry_limit: int
+    trace_rewrites: bool
+    save_directory: Path | None
+    reinit: bool
+    use_booster: bool
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        llvm_dir_str = os.getenv('KMIR_LLVM_DIR')
+        llvm_dir = Path(llvm_dir_str) if llvm_dir_str is not None else None
+        haskell_dir_str = os.getenv('KMIR_HASKELL_DIR')
+        haskell_dir = Path(haskell_dir_str) if haskell_dir_str is not None else None
+
+        return {
+            'definition_dir': llvm_dir,
+            'haskell_dir': haskell_dir,
+            'claim_list': False,
+            'claim': None,
+            'bug_report': False,
+            'depth': None,
+            'smt_timeout': 200,
+            'smt_retry_limit': 4,
+            'trace_rewrites': False,
+            'save_directory': None,
+            'reinit': False,
+            'use_booster': False,  # TODO: debug the booster backend, when it is as fast as legacy, change this to be True
+        }
+
+
 def prove(
     kmir: KMIR,
-    spec_file: Path,
-    *,
-    claim_label: Optional[str] = None,
-    save_directory: Optional[Path] = None,
-    reinit: bool = False,
-    depth: Optional[int] = None,
-    smt_timeout: Optional[int] = None,
-    smt_retry_limit: Optional[int] = None,
-    trace_rewrites: bool = False,
+    options: ProveOptions,
 ) -> tuple[list[Proof], list[Proof]]:
     _LOGGER.info('Extracting claims from file')
 
@@ -49,18 +81,20 @@ def prove(
     else:
         raise ValueError('The prover object in kmir is not initialised.')
 
-    claims = kmir_prover.get_all_claims(spec_file, claim_label)
-    assert claims, ValueError(f'No claims found in file {spec_file}')
+    claims = kmir_prover.get_all_claims(options.spec_file, options.claim)
+    assert claims, ValueError(f'No claims found in file {options.spec_file}')
 
     passing: list[Proof] = []
     failing: list[Proof] = []
     for claim in claims:
         # start an rpc session with KoreServer
-        server = kmir_prover.set_kore_server(smt_timeout=smt_timeout, smt_retry_limit=smt_retry_limit)
+        server = kmir_prover.set_kore_server(smt_timeout=options.smt_timeout, smt_retry_limit=options.smt_retry_limit)
 
-        with kmir_prover.rpc_session(server, claim.label, trace_rewrites) as session:
-            proof = kmir_prover.initialise_a_proof(claim, session, save_directory=save_directory, reinit=reinit)
-            res = kmir.prove_driver(proof, session, max_depth=depth)
+        with kmir_prover.rpc_session(server, claim.label, options.trace_rewrites) as session:
+            proof = kmir_prover.initialise_a_proof(
+                claim, session, save_directory=options.save_directory, reinit=options.reinit
+            )
+            res = kmir.prove_driver(proof, session, max_depth=options.depth)
 
             _, passed = res
             if passed == 'failed':
@@ -70,20 +104,29 @@ def prove(
     return (passing, failing)
 
 
+class ShowProofOptions(LoggingOptions, KCFGShowProofOptions):
+    claim_label: str
+    proof_dir: Path | None
+    definition_dir: Path | None
+    haskell_dir: Path | None
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        llvm_dir_str = os.getenv('KMIR_LLVM_DIR')
+        llvm_dir = Path(llvm_dir_str) if llvm_dir_str is not None else None
+        haskell_dir_str = os.getenv('KMIR_HASKELL_DIR')
+        haskell_dir = Path(haskell_dir_str) if haskell_dir_str is not None else None
+
+        return {
+            'proof_dir': None,
+            'definition_dir': llvm_dir,
+            'haskell_dir': haskell_dir,
+        }
+
+
 def show_proof(
     kmir: KMIR,
-    claim_label: str,
-    proof_dir: Path,
-    nodes: Iterable[NodeIdLike] = (),
-    node_deltas: Iterable[tuple[NodeIdLike, NodeIdLike]] = (),
-    to_module: bool = False,
-    # minimize: bool = True,
-    failure_info: bool = False,
-    # sort_collections: bool = False,
-    pending: bool = False,
-    failing: bool = False,
-    counterexample_info: bool = False,
-    **kwargs: Any,
+    options: ShowProofOptions,
 ) -> str:
     prover = kmir.prover
 
@@ -95,30 +138,34 @@ def show_proof(
         kprove = prover.mir_prove
     # better error message with instructions to fix
 
-    proof = Proof.read_proof_data(proof_dir, claim_label)
+    if options.proof_dir is None:
+        raise ValueError('Proof directory not specified. Must specify with --proof-dir.')
+
+    proof = Proof.read_proof_data(options.proof_dir, options.claim_label)
     # TODO: Create NodePrinter ???
 
     res_lines: list[str] = []
 
     if isinstance(proof, APRProof):
         kmir_show = APRProofShow(kprove)
-        if pending:
-            nodes = list(nodes) + [node.id for node in proof.pending]
-        if failing:
-            nodes = list(nodes) + [node.id for node in proof.failing]
+        nodes = list(options.nodes)
+        if options.pending:
+            nodes += [node.id for node in proof.pending]
+        if options.failing:
+            nodes += [node.id for node in proof.failing]
 
         res_lines += kmir_show.show(
             proof,
             nodes=nodes,
-            node_deltas=node_deltas,
-            to_module=to_module,
+            node_deltas=options.node_deltas,
+            to_module=options.to_module,
             # minimize=minimize,
             # sort_collections=sort_collections,
         )
 
-        if failure_info:
-            with prover.rpc_session(prover.set_kore_server(), claim_label) as kcfg_explore:
-                failures = APRFailureInfo.from_proof(proof, kcfg_explore, counterexample_info)
+        if options.failure_info:
+            with prover.rpc_session(prover.set_kore_server(), options.claim_label) as kcfg_explore:
+                failures = APRFailureInfo.from_proof(proof, kcfg_explore, options.counterexample_info)
                 res_lines += failures.print()
     else:  # TODO: implement the other proof types
         raise ValueError('Proof type not supported yet.')
@@ -126,12 +173,27 @@ def show_proof(
     return '\n'.join(res_lines)
 
 
-def view_proof(
-    kmir: KMIR,
-    claim_label: str,
-    proof_dir: Path,
-    **kwargs: Any,
-) -> None:
+class ViewProofOptions(LoggingOptions):
+    claim_label: str
+    proof_dir: Path | None
+    definition_dir: Path | None
+    haskell_dir: Path | None
+
+    @staticmethod
+    def default() -> dict[str, Any]:
+        llvm_dir_str = os.getenv('KMIR_LLVM_DIR')
+        llvm_dir = Path(llvm_dir_str) if llvm_dir_str is not None else None
+        haskell_dir_str = os.getenv('KMIR_HASKELL_DIR')
+        haskell_dir = Path(haskell_dir_str) if haskell_dir_str is not None else None
+
+        return {
+            'proof_dir': None,
+            'definition_dir': llvm_dir,
+            'haskell_dir': haskell_dir,
+        }
+
+
+def view_proof(kmir: KMIR, options: ViewProofOptions) -> None:
     # TODO: include dirs
 
     prover = kmir.prover
@@ -143,7 +205,10 @@ def view_proof(
     else:
         kprove = prover.mir_prove
 
-    proof = Proof.read_proof_data(proof_dir, claim_label)
+    if options.proof_dir is None:
+        raise ValueError('Proof directory not specified. Must specify with --proof-dir.')
+
+    proof = Proof.read_proof_data(options.proof_dir, options.claim_label)
 
     if isinstance(proof, APRProof):
         # TODO: NodePrinter ???
