08f32da310e5a290cebfb45807d6fa98e8708bdc
Bruce Collie <brucecollie82@gmail.com>
2024-06-25 10:58:15 +0100

    Stub changes to TY into existing test

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -14,13 +14,16 @@ if TYPE_CHECKING:
 CONVERT_BODY_DATA = (Path(__file__).parent / 'data' / 'convert-body').resolve(strict=True)
 CONVERT_BODY_INPUT_DIRS = [CONVERT_BODY_DATA / 'panic']
 
+RUN_PANIC_DATA = (Path(__file__).parent / 'data' / 'run-panic').resolve(strict=True)
+RUN_PANIC_INPUT = [RUN_PANIC_DATA / 'simple.kmir']
+
 
 @pytest.mark.parametrize(
     'test_dir',
     CONVERT_BODY_INPUT_DIRS,
     ids=[str(test_file.relative_to(CONVERT_BODY_DATA)) for test_file in CONVERT_BODY_INPUT_DIRS],
 )
-def test_parse_body(test_dir: Path, tools: Tools) -> None:
+def test_convert_body(test_dir: Path, tools: Tools) -> None:
     serialized_json = test_dir / 'serialized.json'
     reference_mir = test_dir / 'reference.kmir'
 
@@ -30,3 +33,13 @@ def test_parse_body(test_dir: Path, tools: Tools) -> None:
     rc, parsed_ast = tools.kparse.kparse(reference_mir, sort='Body')
 
     assert converted_ast == parsed_ast
+
+
+@pytest.mark.parametrize(
+    'test_file',
+    RUN_PANIC_INPUT,
+    ids=[str(test_file.relative_to(RUN_PANIC_DATA)) for test_file in RUN_PANIC_INPUT],
+)
+def test_run_panic(test_file: Path, tools: Tools) -> None:
+    pass
+    # assert test_file == 1
