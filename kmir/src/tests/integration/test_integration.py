5861a9e72f7beb11e319058586770e31182894f4
Maria Kotsifakou <kotsifakou.maria@gmail.com>
2024-08-07 13:23:33 -0500

    Removed unused testing of deprecated convert.py script.

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -10,33 +10,11 @@ from pyk.kast.inner import KApply, KLabel, KSequence, KSort, KToken
 
 from kmir.convert_from_definition.v2parser import Parser
 
-# from kmir.convert_json.convert import from_dict
-
 if TYPE_CHECKING:
     from pyk.kast.inner import KInner
 
     from kmir.convert_from_definition.v2parser import JSON
     from kmir.tools import Tools
-
-# CONVERT_BODY_DATA = (Path(__file__).parent / 'data' / 'convert-body').resolve(strict=True)
-# CONVERT_BODY_INPUT_DIRS = [CONVERT_BODY_DATA / 'panic']
-#
-#
-# @pytest.mark.parametrize(
-#    'test_dir',
-#    CONVERT_BODY_INPUT_DIRS,
-#    ids=[str(test_file.relative_to(CONVERT_BODY_DATA)) for test_file in CONVERT_BODY_INPUT_DIRS],
-# )
-# def test_convert_body(test_dir: Path, tools: Tools) -> None:
-#    serialized_json = test_dir / 'serialized.json'
-#    reference_mir = test_dir / 'reference.kmir'
-#
-#    with serialized_json.open('r') as f:
-#        converted_ast = from_dict(json.load(f))
-#
-#    rc, parsed_ast = tools.kparse.kparse(reference_mir, sort='Pgm')
-#
-#    assert converted_ast == parsed_ast
 
 
 SCHEMA_PARSE_DATA = (Path(__file__).parent / 'data' / 'schema-parse').resolve(strict=True)
