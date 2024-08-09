ca041f12fe07bd1f3757b2abecc73fc579cf1d11
mariaKt <kotsifakou.maria@gmail.com>
2024-08-09 15:02:25 -0500

    Parser usability and bug fixes (#401)
    * Added information to parser notes.
    
    * Changed parser notes to markdown.
    
    * Added testing path that uses kparse.
    
    This enables writing tests without having to write the AST directly.
    Therefore, it is easier to write larger tests.
    
    * Removed unused testing of deprecated convert.py script.
    
    * Additional tests.
    
    * Changed symbols for lists in body.k according to parser requirements.
    
    * Added missing files for previously added tests.
    
    * Bug fix (list arguments were reversed), and additional tests.
    
    * Make parser use sort instead of symbol as input.
    
    * Additional tests - BasicBlock.
    
    * Changed __main__.py argument name and description from symbol to sort.
    
    * Removed change from symbol input to sort from parser notes.md.
    
    * Bug fix, with additional tests. The bug was exposed with the test for operand.
    
    Single sort enum productions with no named fields were handled wrongly.
    The parser assumed that the detected dictionary was the enum's named
    arguments, when it actually was the single, unnamed argument.
    
    * Additional tests.
    
    * Bug fix, with test that triggered the bug.
    
    The issue was not including None in the potential types of Json
    input to be handled. The rest of the code was already handling None.
    
    * Added comments in parser.
    
    * Removed commented out rules from ty.k
    
    I will include in the PR description.
    
    * Set Version: 0.3.31
    
    * Removed commented out rules from kmir.k.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- kmir/src/kmir/convert_from_definition/__main__.py
+++ kmir/src/kmir/convert_from_definition/__main__.py
@@ -10,7 +10,7 @@ from .v2parser import Parser
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('json', metavar='JSON', help='JSON data to convert')
-    parser.add_argument('symbol', metavar='SYMBOL', help='Symbol for initial production to apply', default='pgm')
+    parser.add_argument('sort', metavar='SORT', help='Expected Sort name for the parsed term', default='pgm')
     return parser.parse_args()
 
 
@@ -19,7 +19,7 @@ def main() -> None:
     tools = semantics()
     p = Parser(tools.definition)
 
-    result = p.parse_mir_json(json.loads(args.json), args.symbol)
+    result = p.parse_mir_json(json.loads(args.json), args.sort)
     if result is None:
         print('Parse error!', file=sys.stderr)
         sys.exit(1)
--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-# import json
+import json
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -10,36 +10,63 @@ from pyk.kast.inner import KApply, KLabel, KSequence, KSort, KToken
 
 from kmir.convert_from_definition.v2parser import Parser
 
-# from kmir.convert_json.convert import from_dict
-
 if TYPE_CHECKING:
     from pyk.kast.inner import KInner
 
     from kmir.convert_from_definition.v2parser import JSON
     from kmir.tools import Tools
 
-# CONVERT_BODY_DATA = (Path(__file__).parent / 'data' / 'convert-body').resolve(strict=True)
-# CONVERT_BODY_INPUT_DIRS = [CONVERT_BODY_DATA / 'panic']
-#
-#
-# @pytest.mark.parametrize(
-#    'test_dir',
-#    CONVERT_BODY_INPUT_DIRS,
-#    ids=[str(test_file.relative_to(CONVERT_BODY_DATA)) for test_file in CONVERT_BODY_INPUT_DIRS],
-# )
-# def test_convert_body(test_dir: Path, tools: Tools) -> None:
-#    serialized_json = test_dir / 'serialized.json'
-#    reference_mir = test_dir / 'reference.kmir'
-#
-#    with serialized_json.open('r') as f:
-#        converted_ast = from_dict(json.load(f))
-#
-#    rc, parsed_ast = tools.kparse.kparse(reference_mir, sort='Pgm')
-#
-#    assert converted_ast == parsed_ast
+
+SCHEMA_PARSE_DATA = (Path(__file__).parent / 'data' / 'schema-parse').resolve(strict=True)
+SCHEMA_PARSE_INPUT_DIRS = [
+    SCHEMA_PARSE_DATA / 'local',
+    SCHEMA_PARSE_DATA / 'maybelocal',
+    SCHEMA_PARSE_DATA / 'span',
+    SCHEMA_PARSE_DATA / 'statement',
+    SCHEMA_PARSE_DATA / 'basicblockidx',
+    SCHEMA_PARSE_DATA / 'maybebasicblockidx',
+    SCHEMA_PARSE_DATA / 'terminatorkindreturn',
+    SCHEMA_PARSE_DATA / 'terminatorkindgoto',
+    SCHEMA_PARSE_DATA / 'terminatorreturn',
+    SCHEMA_PARSE_DATA / 'terminatorgoto',
+    SCHEMA_PARSE_DATA / 'statements',
+    SCHEMA_PARSE_DATA / 'basicblock1',
+    SCHEMA_PARSE_DATA / 'basicblock2',
+    SCHEMA_PARSE_DATA / 'place',
+    SCHEMA_PARSE_DATA / 'operand',
+    SCHEMA_PARSE_DATA / 'rvaluecast',
+    SCHEMA_PARSE_DATA / 'rvalueaggregate',
+    SCHEMA_PARSE_DATA / 'statementassign1',
+    SCHEMA_PARSE_DATA / 'statementassign2',
+    SCHEMA_PARSE_DATA / 'aggregatekindadt',
+]
 
 
-SCHEMA_PARSE_DATA = [
+@pytest.mark.parametrize(
+    'test_dir',
+    SCHEMA_PARSE_INPUT_DIRS,
+    ids=[str(test_file.relative_to(SCHEMA_PARSE_DATA)) for test_file in SCHEMA_PARSE_INPUT_DIRS],
+)
+def test_schema_parse(test_dir: Path, tools: Tools) -> None:
+    input_json = test_dir / 'input.json'
+    reference_sort = test_dir / 'reference.sort'
+    reference_kmir = test_dir / 'reference.kmir'
+    parser = Parser(tools.definition)
+
+    with input_json.open('r') as f:
+        json_data = json.load(f)
+    with reference_sort.open('r') as f:
+        reference_sort_data = f.read().rstrip()
+    parser_result = parser.parse_mir_json(json_data, reference_sort_data)
+    assert parser_result is not None
+    converted_ast, _ = parser_result
+
+    rc, parsed_ast = tools.kparse.kparse(reference_kmir, sort=reference_sort_data)
+
+    assert converted_ast == parsed_ast
+
+
+SCHEMA_PARSE_KAPPLY_DATA = [
     #    ({'Int': 'Isize'}, KApply('IntTy::Isize'), KSort('IntTy')),
     #    ({'Int': 'I8'}, KApply('IntTy::I8'), KSort('IntTy')),
     #    ({'Int': 'I16'}, KApply('IntTy::I16'), KSort('IntTy')),
@@ -60,19 +87,17 @@ SCHEMA_PARSE_DATA = [
     #    ({'RigidTy': {'Int': 'I8'}}, KApply('RigidTy::Int', (KApply('IntTy::I8'))), KSort('RigidTy')),
     #    ({'RigidTy': {'Uint': 'Usize'}}, KApply('RigidTy::Uint', (KApply('UintTy::Usize'))), KSort('RigidTy')),
     #    ({'RigidTy': {'Float': 'F128'}}, KApply('RigidTy::Float', (KApply('FloatTy::F128'))), KSort('RigidTy')),
-    (2, 'local(_)_BODY_Local_Int', KApply('local(_)_BODY_Local_Int', (KToken('2', KSort('Int')))), KSort('Local')),
+    (2, KApply('local(_)_BODY_Local_Int', (KToken('2', KSort('Int')))), KSort('Local')),
     (
         {'StorageLive': 2},
-        'StatementKind::StorageLive',
         KApply('StatementKind::StorageLive', (KApply('local(_)_BODY_Local_Int', (KToken('2', KSort('Int')))))),
         KSort('StatementKind'),
     ),
-    ('Not', 'Mutability::Not', KApply('Mutability::Not', ()), KSort('Mutability')),
-    (2, 'span(_)_TYPES_Span_Int', KApply('span(_)_TYPES_Span_Int', (KToken('2', KSort('Int')))), KSort('Span')),
-    (9, 'ty(_)_TYPES_Ty_Int', KApply('ty(_)_TYPES_Ty_Int', (KToken('9', KSort('Int')))), KSort('Ty')),
+    ('Not', KApply('Mutability::Not', ()), KSort('Mutability')),
+    (2, KApply('span(_)_TYPES_Span_Int', (KToken('2', KSort('Int')))), KSort('Span')),
+    (9, KApply('ty(_)_TYPES_Ty_Int', (KToken('9', KSort('Int')))), KSort('Ty')),
     (
         {'mutability': 'Mut', 'span': 420, 'ty': 9},
-        'localDecl(_,_,_)_BODY_LocalDecl_Ty_Span_Mutability',
         KApply(
             'localDecl(_,_,_)_BODY_LocalDecl_Ty_Span_Mutability',
             (
@@ -88,18 +113,18 @@ SCHEMA_PARSE_DATA = [
 
 @pytest.mark.parametrize(
     'test_case',
-    SCHEMA_PARSE_DATA,
-    ids=[f'{sort.name}-{i}' for i, (_, _, _, sort) in enumerate(SCHEMA_PARSE_DATA)],
+    SCHEMA_PARSE_KAPPLY_DATA,
+    ids=[f'{sort.name}-{i}' for i, (_, _, sort) in enumerate(SCHEMA_PARSE_KAPPLY_DATA)],
 )
-def test_schema_parse(
-    test_case: tuple[JSON, str, KInner, KSort],
+def test_schema_kapply_parse(
+    test_case: tuple[JSON, KInner, KSort],
     tools: Tools,
 ) -> None:
     parser = Parser(tools.definition)
 
-    json_data, symbol, expected_term, expected_sort = test_case
+    json_data, expected_term, expected_sort = test_case
 
-    assert parser.parse_mir_json(json_data, symbol) == (expected_term, expected_sort)
+    assert parser.parse_mir_json(json_data, expected_sort.name) == (expected_term, expected_sort)
 
 
 RUN_PANIC_DATA = (Path(__file__).parent / 'data' / 'run-panic').resolve(strict=True)
