7024cb20a371fe09665d7308a1a1b351c2d8735e
Sam Balco <goodlyrottenapple@gmail.com>
2024-09-07 18:42:27 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,27 @@
 {
   description = "mir-semantics - ";
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.1.127";
+    k-framework.url = "github:runtimeverification/k/v7.1.136";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/k/v7.1.127?dir=pyk";
-    nixpkgs-pyk.follows = "pyk/nixpkgs";
-    poetry2nix.follows = "pyk/poetry2nix";
+    poetry2nix.follows = "k-framework/poetry2nix";
   };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, k-framework, ... }@inputs:
     let
       allOverlays = [
-        poetry2nix.overlay
-        (final: prev: {
-          mir-semantics = prev.poetry2nix.mkPoetryApplication {
+        k-framework.overlays.pyk
+        (final: prev: let
+          poetry2nix =
+            inputs.poetry2nix.lib.mkPoetry2Nix { pkgs = prev; };
+          in {
+          mir-semantics = poetry2nix.mkPoetryApplication {
             python = prev.python310;
-            projectDir = ./.;
+            projectDir = ./kmir;
+            overrides = poetry2nix.overrides.withDefaults
+              (finalPython: prevPython: {
+                kframework = prev.pyk-python310;
+              });
             groups = [];
             # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
             checkGroups = [];
