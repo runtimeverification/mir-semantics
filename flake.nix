9214429e60d31f7e52d856d0c7c9e3fb9a79956a
devops <devops@runtimeverification.com>
2024-09-25 16:07:49 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "mir-semantics - ";
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.1.149";
+    k-framework.url = "github:runtimeverification/k/v7.1.150";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
