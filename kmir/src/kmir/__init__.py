2e6c1b8b498f09f0feb0a682dde0519c83fefc67
gtrepta <50716988+gtrepta@users.noreply.github.com>
2025-04-14 13:29:05 -0500

    Update `kmir run` to behave more like `cargo run` (#524)
    - Introduce `CargoProject` class which can be used to gather information
    about a cargo project.
    - `kmir run` now uses `CargoProject` which finds the `.smir.json`
    artifact in the target directory after invoking `cargo build`.
    
    Note that currently either `RUSTC` will need to be set to the
    `stable-mir-json` install, or the smir json files should already have
    been built that way and exist in the target directory. Later, we can get
    rid of this requirement when we package `stable-mir-json` with `kmir`.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Jost Berthold <jost.berthold@gmail.com>

--- kmir/src/kmir/__init__.py
+++ kmir/src/kmir/__init__.py
@@ -1,3 +1,3 @@
 from typing import Final
 
-VERSION: Final = '0.3.117'
+VERSION: Final = '0.3.118'
--- kmir/src/kmir/__main__.py
+++ kmir/src/kmir/__main__.py
@@ -16,6 +16,7 @@ from pyk.proof.tui import APRProofViewer
 from kmir.build import HASKELL_DEF_DIR, LLVM_LIB_DIR, haskell_semantics, llvm_semantics
 from kmir.kmir import KMIR, KMIRAPRNodePrinter
 from kmir.parse.parser import parse_json
+from kmir.rust import CargoProject
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -27,7 +28,8 @@ class KMirOpts: ...
 
 @dataclass
 class RunOpts(KMirOpts):
-    input_file: Path
+    bin: str | None
+    file: str | None
     depth: int
     start_symbol: str
     haskell_backend: bool
@@ -80,7 +82,15 @@ class ProveViewOpts(KMirOpts):
 def _kmir_run(opts: RunOpts) -> None:
     tools = haskell_semantics() if opts.haskell_backend else llvm_semantics()
 
-    parse_result = parse_json(tools.definition, opts.input_file, 'Pgm')
+    smir_file: Path
+    if opts.file:
+        smir_file = Path(opts.file).resolve()
+    else:
+        cargo = CargoProject(Path.cwd())
+        target = opts.bin if opts.bin else cargo.default_target
+        smir_file = cargo.smir_for(target)
+
+    parse_result = parse_json(tools.definition, smir_file, 'Pgm')
     if parse_result is None:
         print('Parse error!', file=sys.stderr)
         sys.exit(1)
@@ -170,7 +180,9 @@ def _arg_parser() -> ArgumentParser:
     command_parser = parser.add_subparsers(dest='command', required=True)
 
     run_parser = command_parser.add_parser('run', help='run stable MIR programs')
-    run_parser.add_argument('input_file', metavar='FILE', help='MIR program to run')
+    run_target_selection = run_parser.add_mutually_exclusive_group()
+    run_target_selection.add_argument('--bin', metavar='TARGET', help='Target to run')
+    run_target_selection.add_argument('--file', metavar='SMIR', help='SMIR json file to execute')
     run_parser.add_argument('--depth', type=int, metavar='DEPTH', help='Depth to execute')
     run_parser.add_argument(
         '--start-symbol', type=str, metavar='SYMBOL', default='main', help='Symbol name to begin execution from'
@@ -213,7 +225,8 @@ def _parse_args(args: Sequence[str]) -> KMirOpts:
     match ns.command:
         case 'run':
             return RunOpts(
-                input_file=Path(ns.input_file).resolve(),
+                bin=ns.bin,
+                file=ns.file,
                 depth=ns.depth,
                 start_symbol=ns.start_symbol,
                 haskell_backend=ns.haskell_backend,
