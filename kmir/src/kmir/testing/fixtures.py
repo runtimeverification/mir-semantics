a9c668ffd1443578db99579e9664a123caf4574b
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2025-05-01 02:27:14 +0000

    Revert "kmir/{testing/fixtures,integration/test_integration}: re-parametrize test"
    This reverts commit 66b2fec64f26cdaba79546d9192914b06e0bb901.

--- kmir/src/kmir/testing/fixtures.py
+++ kmir/src/kmir/testing/fixtures.py
@@ -2,15 +2,10 @@ from __future__ import annotations
 
 import pytest
 
-from kmir.build import HASKELL_DEF_DIR, LLVM_DEF_DIR, LLVM_LIB_DIR
+from kmir.build import HASKELL_DEF_DIR, LLVM_LIB_DIR
 from kmir.kmir import KMIR
 
 
 @pytest.fixture
-def kmir_haskell() -> KMIR:
+def kmir() -> KMIR:
     return KMIR(HASKELL_DEF_DIR, LLVM_LIB_DIR)
-
-
-@pytest.fixture
-def kmir_llvm() -> KMIR:
-    return KMIR(LLVM_DEF_DIR)
