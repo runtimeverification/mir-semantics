5aef7a50a0cf59a3b510f38521e774003450a03a
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2025-05-01 15:02:19 -0400

    Faster rerun on already passing proofs (#553)
    This PR makes sure that we are not starting up the RPC server unless
    it's actually needed, and also not calling cargo again unless actually
    needed.
    
    In particular:
    
    - The call to `cargo ...` which produces SMIR for building a proof only
    runs if we are actually in the branch which will use it in
    `KMIR.prove_rs`.
    - If the proof we load from disk is already passing, we don't bother
    starting up the `KCFGExplore` (which starts up the booster server).
    - Some formatting cleanups.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Jost Berthold <jost.berthold@gmail.com>

--- kmir/src/kmir/__main__.py
+++ kmir/src/kmir/__main__.py
@@ -43,11 +43,9 @@ def _kmir_run(opts: RunOpts) -> None:
     if parse_result is None:
         print('Parse error!', file=sys.stderr)
         sys.exit(1)
-
     kmir_kast, _ = parse_result
 
     result = kmir.run_parsed(kmir_kast, opts.start_symbol, opts.depth)
-
     print(kmir.kore_to_pretty(result))
 
 
@@ -94,7 +92,7 @@ def _kmir_prove_run(opts: ProveRunOpts) -> None:
             _LOGGER.info(f'Reading proof from disc: {opts.proof_dir}, {label}')
             proof = APRProof.read_proof_data(opts.proof_dir, label)
         else:
-            _LOGGER.info(f'Initialising proof: {label}')
+            _LOGGER.info(f'Constructing initial proof: {label}')
             proof = APRProof.from_claim(kmir.definition, claim, {}, proof_dir=opts.proof_dir)
         with kmir.kcfg_explore(label) as kcfg_explore:
             prover = APRProver(kcfg_explore, execute_depth=opts.max_depth)
@@ -105,23 +103,16 @@ def _kmir_prove_run(opts: ProveRunOpts) -> None:
 
 def _kmir_prove_view(opts: ProveViewOpts) -> None:
     kmir = KMIR(HASKELL_DEF_DIR, LLVM_LIB_DIR)
-
     proof = APRProof.read_proof_data(opts.proof_dir, opts.id)
-
     node_printer = KMIRAPRNodePrinter(kmir, proof)
-
     viewer = APRProofViewer(proof, kmir, node_printer=node_printer)
-
     viewer.run()
 
 
 def _kmir_prove_prune(opts: ProvePruneOpts) -> None:
     proof = APRProof.read_proof_data(opts.proof_dir, opts.id)
-
     pruned_nodes = proof.prune(opts.node_id)
-
     print(f'Pruned nodes: {pruned_nodes}')
-
     proof.write_proof_data()
 
 
