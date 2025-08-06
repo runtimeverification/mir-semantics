c135c21d8464a147a5b7d3246a28322a7b0ff719
Julian Kuners <julian.kuners@gmail.com>
2025-08-07 00:01:55 +0200

    adapt flake and add stable-mir-json

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
   description = "kmir - ";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
     rv-nix-tools.follows = "k-framework/rv-nix-tools";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    stable-mir-json-flake.url = "github:runtimeverification/stable-mir-json/8dcacda4d94f10ea102884887e56da335e4d161c";
+    stable-mir-json-flake = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
     k-framework.url = "github:runtimeverification/k/v7.1.286";
+    k-framework = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     uv2nix.url = "github:pyproject-nix/uv2nix/680e2f8e637bc79b84268949d2f2b2f5e5f1d81c";
     # stale nixpkgs is missing the alias `lib.match` -> `builtins.match`
@@ -21,22 +32,32 @@
     };
     pyproject-nix.follows = "uv2nix/pyproject-nix";
   };
-  outputs = { self, nixpkgs, rv-nix-tools, flake-utils, k-framework, pyproject-nix, pyproject-build-systems, uv2nix }:
+  outputs = {
+    self,
+    nixpkgs,
+    rv-nix-tools,
+    flake-utils,
+    stable-mir-json-flake,
+    k-framework,
+    pyproject-nix,
+    pyproject-build-systems,
+    uv2nix
+  }:
   let
     pythonVer = "310";
-  in flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ] (system:
+  in flake-utils.lib.eachDefaultSystem (system:
     let
-      # due to the nixpkgs that we use in this flake being outdated, uv is also heavily outdated
-      # we can instead use the binary release of uv provided by uv2nix for now
       uvOverlay = final: prev: {
         uv = uv2nix.packages.${final.system}.uv-bin;
       };
-      mir-semanticsOverlay = final: prev: {
+      # create custom overlay for k, because the overlay in k-framework currently also includes a lot of other stuff instead of only k
+      kOverlay = final: prev: {
+        k = k-framework.packages.${final.system}.k;
+      };
+      stable-mir-json-overlay = final: prev: {
+        stable-mir-json = stable-mir-json-flake.packages.${final.system}.stable-mir-json;
+      };
+      mir-semantics-overlay = final: prev: {
         mir-semantics = final.callPackage ./nix/mir-semantics {
           inherit pyproject-nix pyproject-build-systems uv2nix;
           python = final."python${pythonVer}";
@@ -46,23 +67,28 @@
         inherit system;
         overlays = [
           uvOverlay
-          mir-semanticsOverlay
+          kOverlay
+          stable-mir-json-overlay
+          mir-semantics-overlay
         ];
       };
       python = pkgs."python${pythonVer}";
     in {
-      devShells.default = pkgs.mkShell {
+      devShells.default =
+      let
+        stable-mir-json-shell = stable-mir-json-flake.devShells.${system}.default;
+      in pkgs.mkShell {
         name = "uv develop shell";
-        buildInputs = [
+        buildInputs = (stable-mir-json-shell.buildInputs or []) ++ [
           python
           pkgs.uv
         ];
-        env = {
+        env = (stable-mir-json-shell.env or { }) // {
           # prevent uv from managing Python downloads and force use of specific 
           UV_PYTHON_DOWNLOADS = "never";
           UV_PYTHON = python.interpreter;
         };
-        shellHook = ''
+        shellHook = (stable-mir-json-shell.shellHook or "") + ''
           unset PYTHONPATH
         '';
       };
