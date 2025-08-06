cacf7a24c63c3acbdb7e92f5a08414a5499c309e
Julian Kuners <julian.kuners@gmail.com>
2025-08-06 22:42:10 +0200

    cruft update

--- flake.nix
+++ flake.nix
@@ -1,50 +1,78 @@
 {
   description = "kmir - ";
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.1.286";
-    nixpkgs.follows = "k-framework/nixpkgs";
-    flake-utils.follows = "k-framework/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     rv-nix-tools.follows = "k-framework/rv-nix-tools";
-    # poetry2nix.follows = "k-framework/poetry2nix"; # FIXME: To stop crash with `nix flake update`
+
+    flake-utils.url = "github:numtide/flake-utils";
+    k-framework.url = "github:runtimeverification/k/v7.1.286";
+
+    uv2nix.url = "github:pyproject-nix/uv2nix/680e2f8e637bc79b84268949d2f2b2f5e5f1d81c";
+    # stale nixpkgs is missing the alias `lib.match` -> `builtins.match`
+    # therefore point uv2nix to a patched nixpkgs, which introduces this alias
+    # this is a temporary solution until nixpkgs us up-to-date again
+    uv2nix.inputs.nixpkgs.url = "github:runtimeverification/nixpkgs/libmatch";
+    # inputs.nixpkgs.follows = "nixpkgs";
+    pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs/7dba6dbc73120e15b558754c26024f6c93015dd7";
+    pyproject-build-systems = {
+      inputs.nixpkgs.follows = "uv2nix/nixpkgs";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.pyproject-nix.follows = "uv2nix/pyproject-nix";
+    };
+    pyproject-nix.follows = "uv2nix/pyproject-nix";
   };
-  outputs = { self, nixpkgs, flake-utils, k-framework, ... }@inputs:
-    let
-      allOverlays = [
-        k-framework.overlays.pyk
-        (final: prev: let
-          poetry2nix =
-            inputs.poetry2nix.lib.mkPoetry2Nix { pkgs = prev; };
-          in {
-          kmir = poetry2nix.mkPoetryApplication {
-            python = prev.python310;
-            projectDir = ./kmir;
-            overrides = poetry2nix.overrides.withDefaults
-              (finalPython: prevPython: {
-                kframework = prev.pyk-python310;
-              });
-            groups = [];
-            # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
-            checkGroups = [];
-           };
-        })
-      ];
-    in flake-utils.lib.eachSystem [
+  outputs = { self, nixpkgs, rv-nix-tools, flake-utils, k-framework, pyproject-nix, pyproject-build-systems, uv2nix }:
+  let
+    pythonVer = "310";
+  in flake-utils.lib.eachSystem [
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-linux"
       "aarch64-darwin"
     ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = allOverlays;
-        };
-      in {
-        packages = rec {
-          inherit (pkgs) kmir;
-          default = kmir;
-        };
-      }) // {
-        overlay = nixpkgs.lib.composeManyExtensions allOverlays;
+    let
+      # due to the nixpkgs that we use in this flake being outdated, uv is also heavily outdated
+      # we can instead use the binary release of uv provided by uv2nix for now
+      uvOverlay = final: prev: {
+        uv = uv2nix.packages.${final.system}.uv-bin;
       };
+      mir-semanticsOverlay = final: prev: {
+        mir-semantics = final.callPackage ./nix/mir-semantics {
+          inherit pyproject-nix pyproject-build-systems uv2nix;
+          python = final."python${pythonVer}";
+        };
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          uvOverlay
+          mir-semanticsOverlay
+        ];
+      };
+      python = pkgs."python${pythonVer}";
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "uv develop shell";
+        buildInputs = [
+          python
+          pkgs.uv
+        ];
+        env = {
+          # prevent uv from managing Python downloads and force use of specific 
+          UV_PYTHON_DOWNLOADS = "never";
+          UV_PYTHON = python.interpreter;
+        };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+      packages = rec {
+        inherit (pkgs) mir-semantics;
+        default = mir-semantics;
+      };
+    }) // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${final.system}) mir-semantics;
+      };
+    };
 }
