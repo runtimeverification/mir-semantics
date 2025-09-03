d21f33bab2766306b9c7f36e657756e84aeca753
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-09-03 15:09:58 +0000

    Move loop variable inside loop body

--- kmir/src/kmir/linker.py
+++ kmir/src/kmir/linker.py
@@ -19,7 +19,8 @@ def link(smirs: list[SMIRInfo]) -> SMIRInfo:
 
     _LOGGER.info(f'Maximum type ID (offset) is {offset}, linking {len(smirs)} smir.json files')
 
-    for smir, smir_offset in ((smir, offset * i) for i, smir in enumerate(smirs)):
+    for i, smir in enumerate(smirs):
+        smir_offset = offset * i
         _LOGGER.debug(f'Offset {smir_offset} for smir {smir._smir["name"]}')
         apply_offset(smir, smir_offset)
 
