b7584feb7965b4998933a65cd5f850a675aba2c5
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-09-25 15:27:26 +0000

    Refactor the `#decodeValue` test harness
    * Migrate test data from inline K text into JSON files
    * Use `Parser` combined with `kast_to_kore` instead of `_kast` to parse the
      test data (~30x speedup)

--- kmir/src/tests/integration/test_decode_value.py
+++ kmir/src/tests/integration/test_decode_value.py
@@ -9,6 +9,34 @@ if TYPE_CHECKING:
     from pathlib import Path
     from typing import Final
 
+    from pyk.kast.outer import KDefinition
+    from pyk.kore.syntax import Pattern
+
+    from kmir.parse.parser import JSON
+
+
+# Wrap the definition to avoid flooding the output with text from the fixture __repr__ on test failure
+class _KDefinition(NamedTuple):
+    defn: KDefinition
+
+    def __repr__(self) -> str:
+        return repr('KMIR LLVM definition')
+
+
+@pytest.fixture(scope='module')
+def definition_dir() -> Path:
+    from kmir.build import LLVM_DEF_DIR
+
+    return LLVM_DEF_DIR
+
+
+@pytest.fixture(scope='module')
+def definition(definition_dir: Path) -> _KDefinition:
+    from pyk.kast.outer import read_kast_definition
+
+    defn = read_kast_definition(definition_dir / 'compiled.json')
+    return _KDefinition(defn)
+
 
 def dedent(s: str) -> str:
     from textwrap import dedent
@@ -16,153 +44,12 @@ def dedent(s: str) -> str:
     return dedent(s).strip()
 
 
-class _TestData(NamedTuple):
-    test_id: str
-    evaluation: str
-    expected: str
-
-
-TEST_DATA: Final = (
-    _TestData(
-        test_id='bool-false',
-        evaluation=r'#decodeValue(b"\x00", typeInfoPrimitiveType(primTypeBool), .Map)',
-        expected='BoolVal ( false )',
-    ),
-    _TestData(
-        test_id='bool-true',
-        evaluation=r'#decodeValue(b"\x01", typeInfoPrimitiveType(primTypeBool), .Map)',
-        expected='BoolVal ( true )',
-    ),
-    _TestData(
-        test_id='u8',
-        evaluation=r'#decodeValue(b"\xf1", typeInfoPrimitiveType(primTypeUint(uintTyU8)), .Map)',
-        expected='Integer ( 241 , 8 , false )',
-    ),
-    _TestData(
-        test_id='u16',
-        evaluation=r'#decodeValue(b"\xf1\xff", typeInfoPrimitiveType(primTypeUint(uintTyU16)), .Map)',
-        expected='Integer ( 65521 , 16 , false )',
-    ),
-    _TestData(
-        test_id='u32',
-        evaluation=r'#decodeValue(b"\xf1\xff\xff\xff", typeInfoPrimitiveType(primTypeUint(uintTyU32)), .Map)',
-        expected='Integer ( 4294967281 , 32 , false )',
-    ),
-    _TestData(
-        test_id='u64',
-        evaluation=r'#decodeValue(b"\xf1\xff\xff\xff\xff\xff\xff\xff", typeInfoPrimitiveType(primTypeUint(uintTyU64)), .Map)',
-        expected='Integer ( 18446744073709551601 , 64 , false )',
-    ),
-    _TestData(
-        test_id='u128',
-        evaluation=dedent(
-            r"""
-                #decodeValue(
-                    b"\xf1\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
-                    typeInfoPrimitiveType(primTypeUint(uintTyU128)),
-                    .Map
-                )
-            """
-        ),
-        expected='Integer ( 340282366920938463463374607431768211441 , 128 , false )',
-    ),
-    _TestData(
-        test_id='i8',
-        evaluation=r'#decodeValue(b"\xf1", typeInfoPrimitiveType(primTypeInt(intTyI8)), .Map)',
-        expected='Integer ( -15 , 8 , true )',
-    ),
-    _TestData(
-        test_id='i16',
-        evaluation=r'#decodeValue(b"\xf1\xff", typeInfoPrimitiveType(primTypeInt(intTyI16)), .Map)',
-        expected='Integer ( -15 , 16 , true )',
-    ),
-    _TestData(
-        test_id='i32',
-        evaluation=r'#decodeValue(b"\xf1\xff\xff\xff", typeInfoPrimitiveType(primTypeInt(intTyI32)), .Map)',
-        expected='Integer ( -15 , 32 , true )',
-    ),
-    _TestData(
-        test_id='i64',
-        evaluation=r'#decodeValue(b"\xf1\xff\xff\xff\xff\xff\xff\xff", typeInfoPrimitiveType(primTypeInt(intTyI64)), .Map)',
-        expected='Integer ( -15 , 64 , true )',
-    ),
-    _TestData(
-        test_id='i128',
-        evaluation=dedent(
-            r"""
-                #decodeValue(
-                    b"\xf1\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff",
-                    typeInfoPrimitiveType(primTypeInt(intTyI128)),
-                    .Map
-                )
-            """
-        ),
-        expected='Integer ( -15 , 128 , true )',
-    ),
-    _TestData(
-        test_id='array-u8-const-len',
-        evaluation=dedent(
-            r"""
-                #decodeValue(
-                    b"\x00\x01\x02\x03",
-                    typeInfoArrayType(
-                        ty(0),
-                        someTyConst(
-                            tyConst(
-                                tyConstKindValue(
-                                    ty(0),
-                                    allocation(
-                                        b"\x04",
-                                        provenanceMap(.ProvenanceMapEntries),
-                                        align(1),
-                                        mutabilityNot
-                                    )
-                                ),
-                                tyConstId(100)
-                            )
-                        )
-                    ),
-                    ty(0) |-> typeInfoPrimitiveType(primTypeUint(uintTyU8))
-                )
-            """
-        ),
-        expected=dedent(
-            """
-                Range ( ListItem ( Integer ( 0 , 8 , false ) )
-                ListItem ( Integer ( 1 , 8 , false ) )
-                ListItem ( Integer ( 2 , 8 , false ) )
-                ListItem ( Integer ( 3 , 8 , false ) ) )
-            """
-        ),
-    ),
-    _TestData(
-        test_id='array-u8-implicit-len',
-        evaluation=dedent(
-            r"""
-                #decodeValue(
-                    b"\x00\x01\x02\x03",
-                    typeInfoArrayType(ty(0), noTyConst),
-                    ty(0) |-> typeInfoPrimitiveType(primTypeUint(uintTyU8))
-                )
-            """
-        ),
-        expected=dedent(
-            """
-                Range ( ListItem ( Integer ( 0 , 8 , false ) )
-                ListItem ( Integer ( 1 , 8 , false ) )
-                ListItem ( Integer ( 2 , 8 , false ) )
-                ListItem ( Integer ( 3 , 8 , false ) ) )
-            """
-        ),
-    ),
-)
-
 KORE_TEMPLATE: Final = Template(
     dedent(
         r"""
             Lbl'-LT-'generatedTop'-GT-'{}(
                 Lbl'-LT-'kmir'-GT-'{}(
-                    Lbl'-LT-'k'-GT-'{}(kseq{}(inj{SortEvaluation{}, SortKItem{}}($kitem), dotk{}())),
+                    Lbl'-LT-'k'-GT-'{}(kseq{}(inj{SortEvaluation{}, SortKItem{}}($evaluation), dotk{}())),
                     Lbl'-LT-'retVal'-GT-'{}(LblnoReturn'Unds'KMIR-CONFIGURATION'Unds'RetVal{}()),
                     Lbl'-LT-'currentFunc'-GT-'{}(Lblty{}(\dv{SortInt{}}("-1"))),
                     Lbl'-LT-'currentFrame'-GT-'{}(
@@ -187,36 +74,100 @@ KORE_TEMPLATE: Final = Template(
 )
 
 
+class _TestData(NamedTuple):
+    test_id: str
+    bytez: bytes
+    types: dict[int, JSON]
+    type_info: JSON
+    expected: str
+
+    def to_pattern(self, definition: KDefinition) -> Pattern:
+        from pyk.kore.prelude import SORT_K_ITEM, bytes_dv, inj, int_dv, map_pattern
+        from pyk.kore.syntax import App, SortApp
+
+        return App(
+            'LbldecodeValue',
+            (),
+            (
+                bytes_dv(self.bytez),
+                self._json_type_info_to_kore(self.type_info, definition),
+                map_pattern(
+                    *(
+                        (
+                            inj(SortApp('SortTy'), SORT_K_ITEM, App('Lblty', (), (int_dv(key),))),
+                            inj(SortApp('SortTypeInfo'), SORT_K_ITEM, self._json_type_info_to_kore(value, definition)),
+                        )
+                        for key, value in self.types.items()
+                    )
+                ),
+            ),
+        )
+
+    @staticmethod
+    def _json_type_info_to_kore(type_info: JSON, definition: KDefinition) -> Pattern:
+        from pyk.konvert import kast_to_kore
+
+        from kmir.parse.parser import Parser
+
+        parser = Parser(definition)
+        parse_res = parser.parse_mir_json(type_info, 'TypeInfo')
+        assert parse_res
+        term, sort = parse_res
+        return kast_to_kore(definition, term, sort)
+
+
+def load_test_data() -> tuple[_TestData, ...]:
+    from .utils import TEST_DATA_DIR
+
+    test_data_dir = TEST_DATA_DIR / 'decode-value'
+    test_files = sorted(test_data_dir.glob('*.json'))
+    return tuple(parse_test_data(test_file, test_file.with_suffix('.expected')) for test_file in test_files)
+
+
+def parse_test_data(test_file: Path, expected_file: Path) -> _TestData:
+    import json
+
+    test_data = json.loads(test_file.read_text())
+    expected = expected_file.read_text().rstrip()
+
+    return _TestData(
+        test_id=test_file.stem,
+        bytez=bytes(test_data['bytes']),
+        types=dict(test_data['types']),
+        type_info=test_data['typeInfo'],
+        expected=expected,
+    )
+
+
+TEST_DATA: Final = load_test_data()
+
+
 @pytest.mark.parametrize(
     'test_data',
     TEST_DATA,
     ids=[test_id for test_id, *_ in TEST_DATA],
 )
-def test_decode_value(test_data: _TestData, tmp_path: Path) -> None:
+def test_decode_value(
+    test_data: _TestData,
+    definition_dir: Path,
+    definition: _KDefinition,
+    tmp_path: Path,
+) -> None:
     from pyk.kore import match as km
     from pyk.kore.parser import KoreParser
     from pyk.kore.tools import kore_print
     from pyk.ktool.krun import llvm_interpret
     from pyk.utils import chain
 
-    from kmir.build import LLVM_DEF_DIR
-    from kmir.kmir import KMIR
-
     # Given
-    kmir = KMIR(LLVM_DEF_DIR)
-    input_file = tmp_path / 'term.pretty'
-    input_file.write_text(test_data.evaluation)
-    _returncode, kitem = kmir.kparse_into_kore(
-        input_file=input_file,
-        sort='Evaluation',
-    )
-    kore_text = KORE_TEMPLATE.substitute(kitem=kitem.text)
+    evaluation = test_data.to_pattern(definition.defn)
+    kore_text = KORE_TEMPLATE.substitute(evaluation=evaluation.text)
     parser = KoreParser(kore_text)
     init_pattern = parser.pattern()
     assert parser.eof
 
     # When
-    final_pattern = llvm_interpret(definition_dir=LLVM_DEF_DIR, pattern=init_pattern)
+    final_pattern = llvm_interpret(definition_dir=definition_dir, pattern=init_pattern)
     value = (
         chain
         >> km.app("Lbl'-LT-'generatedTop'-GT-'")
@@ -227,7 +178,7 @@ def test_decode_value(test_data: _TestData, tmp_path: Path) -> None:
         >> km.arg(0)
     )(final_pattern)
     actual = kore_print(
-        definition_dir=LLVM_DEF_DIR,
+        definition_dir=definition_dir,
         pattern=value,
         output='pretty',
     )
