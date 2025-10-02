1de4c0c36956a0e839f4e79ba2edde62ba102c5b
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-10-02 15:56:38 +0000

    Implement decoding for arrays

--- kmir/src/kmir/decoding.py
+++ kmir/src/kmir/decoding.py
@@ -8,13 +8,15 @@ if TYPE_CHECKING:
 
 
 def decode_value(data: bytes, type_info: TypeMetadata, types: dict[Ty, TypeMetadata]) -> Value:
-    from .ty import Bool, Int, Uint
+    from .ty import ArrayT, Bool, Int, Uint
 
     match type_info:
         case Bool():
             return _decode_bool(data)
         case Uint(int_ty) | Int(int_ty):
             return _decode_int(data, int_ty)
+        case ArrayT(elem_ty, length):
+            return _decode_array(data, elem_ty, length, types)
         case _:
             raise ValueError(f'Unsupported type: {type_info}')
 
@@ -46,3 +48,31 @@ def _decode_int(data: bytes, int_ty: IntTy | UintTy) -> Value:
         nbits=nbytes * 8,
         signed=signed,
     )
+
+
+def _decode_array(
+    data: bytes,
+    elem_ty: Ty,
+    length: int | None,
+    types: dict[Ty, TypeMetadata],
+) -> Value:
+    from .value import RangeValue
+
+    try:
+        elem_info = types[elem_ty]
+    except KeyError as err:
+        raise ValueError(f'Unknown element type: {elem_ty}') from err
+
+    elem_nbytes = elem_info.nbytes(types)
+
+    elems = []
+    while data:
+        elem_data = data[:elem_nbytes]
+        data = data[elem_nbytes:]
+        elem = decode_value(elem_data, elem_info, types)
+        elems.append(elem)
+
+    if length is not None and len(elems) != length:
+        raise ValueError(f'Expected {length} elements, got: {len(elems)}')
+
+    return RangeValue(elems)
--- kmir/src/kmir/value.py
+++ kmir/src/kmir/value.py
@@ -7,6 +7,8 @@ from typing import TYPE_CHECKING
 from pyk.kast.inner import KApply
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
+
     from pyk.kast import KInner
 
 
@@ -41,3 +43,16 @@ class IntValue(Value):
             intToken(self.nbits),
             boolToken(self.signed),
         )
+
+
+@dataclass
+class RangeValue(Value):
+    elems: tuple[Value, ...]
+
+    def __init__(self, elems: Iterable[Value]):
+        self.elems = tuple(elems)
+
+    def to_kast(self):
+        from pyk.kast.prelude.collections import list_of
+
+        return KApply('Value::Range', list_of(elem.to_kast() for elem in self.elems))
