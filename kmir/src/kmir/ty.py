546f777f09d7bfd13e4e38875aa34d368eb6401f
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-10-09 15:23:17 +0000

    Model `LayoutShape`

--- kmir/src/kmir/ty.py
+++ kmir/src/kmir/ty.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC
 from dataclasses import dataclass
 from enum import Enum
-from typing import TYPE_CHECKING, NewType
+from typing import TYPE_CHECKING, NamedTuple, NewType
 
 if TYPE_CHECKING:
     from collections.abc import Mapping
@@ -126,6 +126,7 @@ class EnumT(TypeMetadata):
     adt_def: int
     discriminants: list[int]
     fields: list[list[Ty]]
+    layout: LayoutShape | None
 
     @staticmethod
     def from_raw(data: Any) -> EnumT:
@@ -136,6 +137,7 @@ class EnumT(TypeMetadata):
                     'adt_def': adt_def,
                     'discriminants': discriminants,
                     'fields': fields,
+                    'layout': layout,
                 }
             }:
                 return EnumT(
@@ -143,9 +145,249 @@ class EnumT(TypeMetadata):
                     adt_def=adt_def,
                     discriminants=list(discriminants),
                     fields=[list(tys) for tys in fields],
+                    layout=LayoutShape.from_raw(layout),
                 )
             case _:
                 raise _cannot_parse_as('EnumT', data)
+
+
+@dataclass
+class LayoutShape:
+    fields: FieldsShape
+    variants: VariantsShape
+    abi: ValueAbi
+    abi_align: int
+    size: MachineSize
+
+    @staticmethod
+    def from_raw(data: Any) -> LayoutShape:
+        match data:
+            case {
+                'fields': fields,
+                'variants': variants,
+                'abi': abi,
+                'abi_align': abi_align,
+                'size': size,
+            }:
+                return LayoutShape(
+                    fields=FieldsShape.from_raw(fields),
+                    variants=VariantsShape.from_raw(variants),
+                    abi=ValueAbi.from_raw(abi),
+                    abi_align=int(abi_align),
+                    size=MachineSize.from_raw(size),
+                )
+            case _:
+                raise _cannot_parse_as('LayoutShape', data)
+
+
+class FieldsShape(ABC):  # noqa: B024
+    @staticmethod
+    def from_raw(data: Any) -> FieldsShape:
+        match data:
+            case 'Primitive':
+                return PrimitiveFields()
+            case {
+                'Arbitrary': {
+                    'offsets': offsets,
+                },
+            }:
+                return ArbitraryFields(
+                    offsets=[MachineSize.from_raw(offset) for offset in offsets],
+                )
+            case _:
+                raise _cannot_parse_as('FieldsShape', data)
+
+
+@dataclass
+class PrimitiveFields(FieldsShape): ...
+
+
+@dataclass
+class ArbitraryFields(FieldsShape):
+    offsets: list[MachineSize]
+
+
+class VariantsShape(ABC):  # noqa: B024
+    @staticmethod
+    def from_raw(data: Any) -> VariantsShape:
+        match data:
+            case {
+                'Single': {
+                    'index': index,
+                },
+            }:
+                return Single(index=index)
+            case {
+                'Multiple': {
+                    'tag': tag,
+                    'tag_encoding': tag_encoding,
+                    'tag_field': tag_field,
+                    'variants': variants,
+                },
+            }:
+                return Multiple(
+                    tag=Scalar.from_raw(tag),
+                    tag_encoding=TagEncoding.from_raw(tag_encoding),
+                    tag_field=int(tag_field),
+                    variants=[LayoutShape.from_raw(variant) for variant in variants],
+                )
+            case _:
+                raise _cannot_parse_as('FieldsShape', data)
+
+
+@dataclass
+class Single(VariantsShape):
+    index: int
+
+
+@dataclass
+class Multiple(VariantsShape):
+    tag: Scalar
+    tag_encoding: TagEncoding
+    tag_field: int
+    variants: list[LayoutShape]
+
+
+@dataclass
+class ValueAbi:
+    @staticmethod
+    def from_raw(data: Any) -> ValueAbi:
+        return ValueAbi()
+
+
+@dataclass
+class MachineSize:
+    num_bits: int
+
+    @staticmethod
+    def from_raw(data: Any) -> MachineSize:
+        match data:
+            case {
+                'num_bits': num_bits,
+            }:
+                return MachineSize(num_bits=num_bits)
+            case _:
+                raise _cannot_parse_as('MachineSize', data)
+
+
+class Scalar(ABC):  # noqa: B024
+    @staticmethod
+    def from_raw(data: Any) -> Scalar:
+        match data:
+            case {
+                'Initialized': {
+                    'value': value,
+                    'valid_range': valid_range,
+                },
+            }:
+                return Initialized(
+                    value=Primitive.from_raw(value),
+                    valid_range=WrappingRange.from_raw(valid_range),
+                )
+            case {
+                'Union': {
+                    'value': value,
+                },
+            }:
+                return Union(
+                    value=Primitive.from_raw(value),
+                )
+            case _:
+                raise _cannot_parse_as('Scalar', data)
+
+
+@dataclass
+class Initialized(Scalar):
+    value: Primitive
+    valid_range: WrappingRange
+
+
+@dataclass
+class Union(Scalar):
+    value: Primitive
+
+
+class Primitive(ABC):  # noqa: B024
+    @staticmethod
+    def from_raw(data: Any) -> Primitive:
+        match data:
+            case {
+                'Int': {
+                    'length': length,
+                    'signed': signed,
+                },
+            }:
+                return PrimitiveInt(
+                    length=IntegerLength[str(length)],
+                    signed=bool(signed),
+                )
+            case {'Float': _}:
+                return Float()
+            case {'Pointer': _}:
+                return Pointer()
+            case _:
+                raise _cannot_parse_as('Primitive', data)
+
+
+@dataclass
+class PrimitiveInt(Primitive):
+    length: IntegerLength
+    signed: bool
+
+
+class IntegerLength(Enum):
+    I8 = 1
+    I16 = 2
+    I32 = 4
+    I64 = 8
+    I128 = 16
+
+
+@dataclass
+class Float(Primitive): ...
+
+
+@dataclass
+class Pointer(Primitive): ...
+
+
+class TagEncoding(ABC):  # noqa: B024
+    @staticmethod
+    def from_raw(data: Any) -> TagEncoding:
+        match data:
+            case 'Direct':
+                return Direct()
+            case {'Niche': _}:
+                return Niche()
+            case _:
+                raise _cannot_parse_as('TagEncoding', data)
+
+
+@dataclass
+class Direct(TagEncoding): ...
+
+
+@dataclass
+class Niche(TagEncoding): ...
+
+
+class WrappingRange(NamedTuple):
+    start: int
+    end: int
+
+    @staticmethod
+    def from_raw(data: Any) -> WrappingRange:
+        match data:
+            case {
+                'start': start,
+                'end': end,
+            }:
+                return WrappingRange(
+                    start=int(start),
+                    end=int(end),
+                )
+            case _:
+                raise _cannot_parse_as('WrappingRange', data)
 
 
 @dataclass
