f0ef7a0805a1ad3b161405433f1bc3652a470222
dkcumming <daniel.cumming@runtimeverification.com>
2025-10-22 20:05:25 +1000

    Added empty for left and RHS

--- kmir/src/tests/integration/data/prove-rs/slice-split-at.rs
+++ kmir/src/tests/integration/data/prove-rs/slice-split-at.rs
@@ -11,10 +11,17 @@ fn main() {
     assert!(left == [42]);
     assert!(right == [255, 66]);
 
-    // // This case currently failing
-    // let arr: [u8; 3] = [42, 255, 66];
-    // let slice = arr.as_slice();
-    // let (left, right) = slice.split_at(3);
-    // assert!(left == [42, 255, 66]);
-    // assert!(right == []);
+    // Empty Case RHS
+    let arr: [u8; 3] = [42, 255, 66];
+    let slice = arr.as_slice();
+    let (left, right) = slice.split_at(3);
+    assert!(left == [42, 255, 66]);
+    assert!(right == []);
+
+    // Empty Case LHS
+    let arr: [u8; 3] = [42, 255, 66];
+    let slice = arr.as_slice();
+    let (left, right) = slice.split_at(0);
+    assert!(left == []);
+    assert!(right == [42, 255, 66]);
 }
