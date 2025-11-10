3b6fb0d2501d3be9b6d612120dd9567eec9d059a
Jost Berthold <jost.berthold@gmail.com>
2025-11-10 14:27:26 +1100

    tinker with program to avoid premature round-trip transmute optimisation

--- kmir/src/tests/integration/data/prove-rs/transmute-bytes.rs
+++ kmir/src/tests/integration/data/prove-rs/transmute-bytes.rs
@@ -5,13 +5,19 @@ use std::mem::transmute;
 
 fn bytes_to_u64(bytes: [u8; 8]) -> u64 {
     let value = unsafe { transmute::<[u8; 8], u64>(bytes) };
-    let roundtrip = unsafe { transmute::<u64, [u8; 8]>(value) };
+    // prevent round-trip-transmute rule from simplifying this
+    let enlarged = value as u128;
+    let original = enlarged as u64;
+    let roundtrip = unsafe { transmute::<u64, [u8; 8]>(original) };
     assert_eq!(roundtrip, bytes);
     value
 }
 
 fn u64_to_bytes(value: u64) -> [u8; 8] {
-    let bytes = unsafe { transmute::<u64, [u8; 8]>(value) };
+    let mut bytes = unsafe { transmute::<u64, [u8; 8]>(value) };
+    let first = bytes[0];
+    bytes[0] = 255u8;
+    bytes[0] = first;
     let roundtrip = unsafe { transmute::<[u8; 8], u64>(bytes) };
     assert_eq!(roundtrip, value);
     bytes
