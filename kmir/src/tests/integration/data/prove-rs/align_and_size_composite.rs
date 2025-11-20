c7bdccd0e7df020bb25d2b38eacfd161ea59e962
Jost Berthold <jost.berthold@gmail.com>
2025-11-20 16:32:30 +1100

    reduce size for align_composite test

--- kmir/src/tests/integration/data/prove-rs/align_and_size_composite.rs
+++ kmir/src/tests/integration/data/prove-rs/align_and_size_composite.rs
@@ -9,12 +9,6 @@ fn main() {
     assert_eq!(size_of::<[u8; 4]>(), 4);
     assert_eq!(align_of::<[u8; 4]>(), 1);
 
-    assert_eq!(size_of::<[u8; 16]>(), 16);
-    assert_eq!(align_of::<[u8; 16]>(), 1);
-
-    assert_eq!(size_of::<[u32; 4]>(), 16);
-    assert_eq!(align_of::<[u32; 4]>(), 4);
-
     assert_eq!(size_of::<[u64; 3]>(), 24);
     assert_eq!(align_of::<[u64; 3]>(), 8);
 
@@ -22,23 +16,11 @@ fn main() {
     assert_eq!(size_of::<()>(), 0);
     assert_eq!(align_of::<()>(), 1);
 
-    assert_eq!(size_of::<(u8,)>(), 1);
-    assert_eq!(align_of::<(u8,)>(), 1);
-
-    assert_eq!(size_of::<(u8, u8)>(), 2);
-    assert_eq!(align_of::<(u8, u8)>(), 1);
-
     assert_eq!(size_of::<(u8, u16)>(), 4);
     assert_eq!(align_of::<(u8, u16)>(), 2);
 
-    assert_eq!(size_of::<(u8, u32)>(), 8);
-    assert_eq!(align_of::<(u8, u32)>(), 4);
-
     assert_eq!(size_of::<(u8, u64)>(), 16);
     assert_eq!(align_of::<(u8, u64)>(), 8);
-
-    assert_eq!(size_of::<(u32, u32)>(), 8);
-    assert_eq!(align_of::<(u32, u32)>(), 4);
 
     assert_eq!(size_of::<(u64, u8, u64)>(), 24);
     assert_eq!(align_of::<(u64, u8, u64)>(), 8);
@@ -99,14 +81,6 @@ fn main() {
     assert_eq!(size_of::<Align16>(), 16);
     assert_eq!(align_of::<Align16>(), 16);
 
-    #[allow(dead_code)]
-    #[repr(align(32))]
-    struct Align32 {
-        a: u32,
-    }
-    assert_eq!(size_of::<Align32>(), 32);
-    assert_eq!(align_of::<Align32>(), 32);
-
     // Enums
     #[allow(dead_code)]
     enum SimpleEnum {
@@ -138,15 +112,9 @@ fn main() {
     assert_eq!(size_of::<Option<u32>>(), 8);
     assert_eq!(align_of::<Option<u32>>(), 4);
 
-    assert_eq!(size_of::<Option<u64>>(), 16);
-    assert_eq!(align_of::<Option<u64>>(), 8);
-
     // Null pointer optimization
     assert_eq!(size_of::<Option<&u32>>(), 8);
     assert_eq!(align_of::<Option<&u32>>(), 8);
-
-    assert_eq!(size_of::<Option<Box<u32>>>(), 8);
-    assert_eq!(align_of::<Option<Box<u32>>>(), 8);
 
     // Result
     assert_eq!(size_of::<Result<u32, u32>>(), 8);
@@ -159,24 +127,5 @@ fn main() {
     struct ZeroSized;
     assert_eq!(size_of::<ZeroSized>(), 0);
     assert_eq!(align_of::<ZeroSized>(), 1);
-
-    struct PhantomStruct<T> {
-        _marker: std::marker::PhantomData<T>,
-    }
-    assert_eq!(size_of::<PhantomStruct<u64>>(), 0);
-    assert_eq!(align_of::<PhantomStruct<u64>>(), 1);
-
-    // String and Vec (heap-allocated)
-    assert_eq!(size_of::<String>(), 24);
-    assert_eq!(align_of::<String>(), 8);
-
-    assert_eq!(size_of::<Vec<u32>>(), 24);
-    assert_eq!(align_of::<Vec<u32>>(), 8);
-
-    assert_eq!(size_of::<Box<u32>>(), 8);
-    assert_eq!(align_of::<Box<u32>>(), 8);
-
-    assert_eq!(size_of::<Box<[u32]>>(), 16);
-    assert_eq!(align_of::<Box<[u32]>>(), 8);
 
 }
