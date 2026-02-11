64f13760bd9c82fa1dd69d88efe0727a35d08051
devops <devops@runtimeverification.com>
2026-02-11 01:39:52 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    stable-mir-json-flake.url = "github:runtimeverification/stable-mir-json/a5b714d89d0c12c7f4b00602a95ad2d3a34530f0";
+    stable-mir-json-flake.url = "github:runtimeverification/stable-mir-json/41c7d552a1be9d9927ccc5a9365636684916b631";
     stable-mir-json-flake = {
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
