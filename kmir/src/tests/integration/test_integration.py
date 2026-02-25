02ed784f96711c918f0617e57e674c6bbcb2c93d
Stevengre <zhaojianhong96@gmail.com>
2026-02-25 21:42:28 +0800

    test(prove-rs): add signer-index red repro with show snapshot

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -38,6 +38,7 @@ PROVE_RS_START_SYMBOLS = {
     'assume-cheatcode-conflict-fail': ['check_assume_conflict'],
     'transmute-bytes': ['bytes_to_u64', 'u64_to_bytes'],
     'test_offset_from-fail': ['testing'],
+    'spl-multisig-signer-index': ['repro'],
 }
 PROVE_RS_SHOW_SPECS = [
     'local-raw-fail',
@@ -62,6 +63,8 @@ PROVE_RS_SHOW_SPECS = [
     'test_offset_from-fail',
     'ref-ptr-cast-elem-fail',
     'ref-ptr-cast-elem-offset-fail',
+    'and_then_closure-fail',
+    'closure_access_struct-fail',
 ]
 
 
