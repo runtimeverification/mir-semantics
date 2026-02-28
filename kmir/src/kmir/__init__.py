f030a40e18f2d5666a67ba34bea110461e1a0feb
devops <devops@runtimeverification.com>
2026-02-28 07:46:50 +0000

    deps/stable-mir-json: sync submodule ff1a18f059ae8fa9c958f61bc5c280e9eceaaae9

--- kmir/src/kmir/__init__.py
+++ kmir/src/kmir/__init__.py
@@ -2,4 +2,4 @@ from importlib.metadata import version
 from typing import Final
 
 __version__: Final = version('kmir')
-__smir_version__: Final = 'cab07e2d7848e887a1cacdb947703b298a99bc69'
+__smir_version__: Final = 'ff1a18f059ae8fa9c958f61bc5c280e9eceaaae9'
