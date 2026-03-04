68a9792ad79b4d65c3f3d4f47b59d36cff4c7d32
mariaKt <kotsifakou.maria@gmail.com>
2026-03-04 16:38:22 -0600

    Test iter_next_3-fail.rs -> iter_next_3.rs.

--- kmir/src/tests/integration/test_integration.py
+++ kmir/src/tests/integration/test_integration.py
@@ -43,7 +43,7 @@ PROVE_RS_SHOW_SPECS = [
     'local-raw-fail',
     'interior-mut-fail',
     'interior-mut3-fail',
-    'iter_next_3-fail',
+    'iter_next_3',
     'assert_eq_exp',
     'bitwise-not-shift',
     'symbolic-args-fail',
