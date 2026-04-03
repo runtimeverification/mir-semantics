c8534539c6ddff26982fd3ef04518c41803015e5
Stevengre <zhaojianhong96@gmail.com>
2026-04-03 13:31:04 +0000

    fix(transmute): accept MaybeUninit reinterpretation

--- kmir/src/tests/integration/data/prove-rs/transmute-maybe-uninit-i128.rs
+++ kmir/src/tests/integration/data/prove-rs/transmute-maybe-uninit-i128.rs
@@ -4,7 +4,6 @@ use std::mem::MaybeUninit;
 fn main() {
     unsafe {
         let maybe_unsigned = std::intrinsics::transmute::<i128, MaybeUninit::<u128>>(9999);
-                                  // Note different types ^^^^                ^^^^
         assert!(maybe_unsigned.assume_init() == 9999);
     }
 }
