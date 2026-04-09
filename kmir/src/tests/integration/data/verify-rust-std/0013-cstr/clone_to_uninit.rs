13bcd7862b21b29befa9682d4ffdd9ee1e2400aa
Stevengre <zhaojianhong96@gmail.com>
2026-04-09 05:31:27 +0000

    feat(vrs-0013): add clone_to_uninit slice harness

---
+++ kmir/src/tests/integration/data/verify-rust-std/0013-cstr/clone_to_uninit.rs
@@ -0,0 +1,50 @@
+#![feature(clone_to_uninit)]
+
+use std::clone::CloneToUninit;
+use std::ffi::CStr;
+
+macro_rules! precondition {
+    ($cond:expr, $body:block) => {
+        if $cond $body
+    };
+}
+
+fn main() {
+    test_clone_to_uninit_exact_bytes();
+}
+
+fn test_clone_to_uninit_exact_bytes() {
+    let source = CStr::from_bytes_with_nul(b"hello\0");
+
+    match source {
+        Ok(cstr) => {
+            let source_bytes = cstr.to_bytes_with_nul();
+            let len = source_bytes.len();
+            let mut dest = [0xAAu8; 6];
+            let dest_ptr = dest.as_mut_ptr();
+
+            precondition!(!dest_ptr.is_null(), {
+                precondition!(len <= dest.len(), {
+                    unsafe {
+                        cstr.clone_to_uninit(dest_ptr);
+                    }
+
+                    let written = &dest[..len];
+
+                    assert!(written.len() == source_bytes.len());
+                    assert!(written[0] == source_bytes[0]);
+                    assert!(written[1] == source_bytes[1]);
+                    assert!(written[2] == source_bytes[2]);
+                    assert!(written[3] == source_bytes[3]);
+                    assert!(written[4] == source_bytes[4]);
+                    assert!(written[5] == source_bytes[5]);
+
+                    let cloned = CStr::from_bytes_with_nul(written);
+                    assert!(cloned.is_ok());
+                    assert!(cloned.unwrap().to_bytes_with_nul() == source_bytes);
+                });
+            });
+        }
+        Err(_) => panic!("expected Ok"),
+    }
+}
