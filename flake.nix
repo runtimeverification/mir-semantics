2a094897a6aa91ad8512b66ec2e07438e32e15b0
devops <devops@runtimeverification.com>
2026-05-07 18:35:00 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
 
-    k-framework.url = "github:runtimeverification/k/v7.1.319";
+    k-framework.url = "github:runtimeverification/k/v7.1.321";
     k-framework = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
