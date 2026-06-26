a1b17d170188b21347ce9301209341f4453b0fc4
mariaKt <kotsifakou.maria@gmail.com>
2026-06-25 19:13:57 -0500

    Added test for min_align_of_val (showing it is stuck)

--- kmir/src/tests/integration/data/prove-rs/intrinsics.rs
+++ kmir/src/tests/integration/data/prove-rs/intrinsics.rs
@@ -66,3 +66,10 @@ pub fn test_size_of_val() {
     let r = unsafe { intrinsics::size_of_val(&x as *const u32) };
     assert!(r == 4);
 }
+
+#[no_mangle]
+pub fn test_min_align_of_val() {
+    let x: u32 = 0;
+    let r = unsafe { intrinsics::min_align_of_val(&x as *const u32) };
+    assert!(r == 4);
+}
